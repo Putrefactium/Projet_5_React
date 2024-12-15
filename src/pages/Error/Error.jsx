bd50b3055677feb931c00e12c89da46dd74f108a
Jérémie Marie <jeremie_marie14@hotmail.fr>
2024-12-15 15:01:51 +0100

    ETAPE 6.0 : Implémentation de la page d'erreur 404
    - Création du composant Error avec style mobile
    - Ajout des styles SCSS pour la mise en page
    - Intégration du Header et Footer
    - Ajout des variables pour la page 404 dans variables.scss
    - Configuration du layout et de la typographie
    
    TODO: Implémenter le responsive tablette et desktop

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,12 +1,24 @@
 import { Link } from 'react-router-dom'
+import Header from '@layout/Header/Header'
+import Footer from '@layout/Footer/Footer'
+import './Error.scss'
 
 function Error() {
   return (
-    <main className="error">
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n&apos;existe pas.</p>
-      <Link to="/">Retourner sur la page d&apos;accueil</Link>
-    </main>
+    <>
+    <div className="main-error">
+    <Header />
+      <main className="error"> 
+        <h1>404</h1>
+        <p>
+          <span>Oups! La page que </span>
+          <span>vous demandez n&apos;existe pas.</span> 
+        </p>
+        <Link to="/">Retourner sur la page d&apos;accueil</Link>
+      </main>
+    </div>
+    <Footer />
+    </>
   )
 }
 
